bf8994077cea1102d553633d263dcea238b9dd2b
moduloWTech <contato.moduloweb@gmail.com>
2026-07-20 15:41:38 -0300

    feat: substitui o bot centralizado do whatsapp pela API oficial de Deep Linking (wa.me)

--- api/src/routes/whatsapp.router.ts
+++ api/src/routes/whatsapp.router.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from 'fastify';
-import { whatsappService } from '../services/whatsapp.service';
+// import { whatsappService } from '../services/whatsapp.service';
 
 export class WhatsappRouter {
   register(app: FastifyInstance) {
     // Rota que retorna o status da conexão e o QR Code, se houver
-    app.get('/api/whatsapp/status', async (request, reply) => {
+    /* app.get('/api/whatsapp/status', async (request, reply) => {
       try {
         const status = whatsappService.getStatus();
         return reply.status(200).send(status);
@@ -12,6 +12,6 @@ export class WhatsappRouter {
         app.log.error(error);
         return reply.status(500).send({ error: 'Falha ao buscar status do WhatsApp' });
       }
-    });
+    }); */
   }
 }
